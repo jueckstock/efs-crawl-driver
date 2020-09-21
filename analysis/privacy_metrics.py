d138083db65dc68305df3e96356f6512ac4119fd
Jordan Jueckstock <jjuecks@ncsu.edu>
2020-09-21 17:38:38 -0400

    blundering along

--- analysis/privacy_metrics.py
+++ analysis/privacy_metrics.py
@@ -18,29 +18,50 @@ from publicsuffix2 import get_sld
 
 from common import graphs_in_dir, walk_experiment_trees
 
-SITE_THRESHOLD = int(os.environ.get("SITE_THRESHOLD", 5))
+SITE_THRESHOLD = int(os.environ.get("SITE_THRESHOLD", 1))
 
 
 def main(argv):
-    if len(argv) < 2:
-        print(f"usage: {argv[0]} PRIVACY_METRICS_CSV [src1 [src2 [...]]]")
+    if len(argv) < 3:
+        print(f"usage: {argv[0]} PRIVACY_METRICS_CSV (lat|long) [src1 [src2 [...]]]")
         return
+    
     privacy_csv = argv[1]
     csv_stem = os.path.splitext(privacy_csv)[0]
-    privacy_pdf = csv_stem + ".pdf"
+
+    mode = argv[2].lower()
+    assert mode in ('lat', 'long'), "Invalid mode!"
+
+    source_tag = "_" + '-'.join(a.lower() for a in argv[3:]) if argv[3:] else ''
+    privacy_pdf = f"{csv_stem}_{mode}{source_tag}.pdf"
     
     df = pd.read_csv(privacy_csv)
-    sources = argv[2:]
+    sources = argv[3:]
     if sources:
         df = df[df.source.isin(sources)]
     df = df.drop('source', axis=1)
 
     counts = df.groupby(['http_etld1', 'key', 'value']).nunique()
-    distinctive_flows = counts[(counts.profile == 1) & (counts.site_etld1 > SITE_THRESHOLD)].index
-    privacy_tokens = df.set_index(['http_etld1', 'key', 'value']).loc[distinctive_flows]
-    trackability = privacy_tokens.reset_index().groupby(['http_etld1', 'profile']).site_etld1.nunique().unstack(fill_value=0)
+    if mode == 'lat':
+        distinctive_flows = counts[(counts.profile == 1) & (counts.site_etld1 > SITE_THRESHOLD)]
+        xfield = 'http_etld1'
+        yfield = 'site_etld1'
+    else:
+        distinctive_flows = counts[(counts.profile == 1) & (counts.session > 1)]
+        xfield = 'site_etld1'
+        yfield = 'http_etld1'
+    #distinctive_flows.to_csv("scratch-flows.csv")
+    
+    privacy_tokens = df.set_index(['http_etld1', 'key', 'value']).loc[distinctive_flows.index]
+    #privacy_tokens.to_csv("scratch-rows.csv")
+    
+    trackability = privacy_tokens.reset_index().groupby([xfield, 'profile'])[yfield].nunique().unstack(fill_value=0)
+    #trackability.to_csv("scratch-tracking.csv")
 
-    ax = trackability.cumsum().plot()
+
+    wut = trackability.cumsum()
+    #print(wut)
+    ax = wut.plot()
     fig = ax.get_figure()
     fig.savefig(privacy_pdf)
 
--- analysis/sim/plot_bagz.py
+++ analysis/sim/plot_bagz.py
@@ -10,6 +10,7 @@ from matplotlib import pyplot as plt
 
 
 T1 = float(os.environ.get("T1", 1.0))
+ROOT = bool(os.environ.get("ROOT", False))
 
 
 def stable0(df: pd.DataFrame) -> pd.DataFrame:
@@ -52,25 +53,38 @@ def main(argv):
     csv_stem = os.path.splitext(csvfile)[0]
     df = pd.read_csv(csvfile)
 
+    if 'is_root' in df.columns:
+        if ROOT:
+            df = df[df.is_root == True].drop(['is_root', 'is_ad'], axis=1)
+            csv_stem += "_root"
+        else:
+            df = df[(df.is_root == False) & (df.is_ad == False)].drop(['is_root', 'is_ad'], axis=1)
+            csv_stem += "_3pnoad"
+    
+    if 'session' in df.columns:
+        groupers = ['session', 'site_tag', 'frame_url', 'p2', 'p1']
+    else:
+        groupers = ['site_tag', 'frame_url', 'p2', 'p1']
+
     YRANGE = (-0.1, 1.1)
 
     for stability_algo, stability_func in STABLES.items():
-        raw_by_node = df.groupby(['site_tag', 'frame_url', 'p2', 'p1']).node_jaccard.sum().unstack().dropna()
+        raw_by_node = df.groupby(groupers).node_jaccard.sum().unstack().dropna()
         by_node = stability_func(raw_by_node)
         node_ratio = len(by_node) / len(raw_by_node)
-        raw_by_edge = df.groupby(['site_tag', 'frame_url', 'p2', 'p1']).edge_jaccard.sum().unstack().dropna()
+        raw_by_edge = df.groupby(groupers).edge_jaccard.sum().unstack().dropna()
         by_edge = stability_func(raw_by_edge)
         edge_ratio = len(by_edge) / len(raw_by_edge)
         
         ax = (by_node.cumsum() / len(by_node)).plot(title=f"Cumulative Node-Bag Similarity [0.0-1.0] Scores\n({stability_algo}[{T1:.2f}]: {node_ratio:.2%})", ylim=YRANGE)
         save_ax_pdf(ax, f"{csv_stem}_nodes_sum_{stability_algo}.pdf")
-        ax = by_node.plot.box(title=f"Node-Bag Similarity [0.0-1.0] Score Distributions\n({stability_algo}[{T1:.2f}]: {node_ratio:.2%})", ylim=YRANGE)
-        save_ax_pdf(ax, f"{csv_stem}_nodes_box_{stability_algo}.pdf", no_xticks=False)
+        #ax = by_node.plot.box(title=f"Node-Bag Similarity [0.0-1.0] Score Distributions\n({stability_algo}[{T1:.2f}]: {node_ratio:.2%})", ylim=YRANGE)
+        #save_ax_pdf(ax, f"{csv_stem}_nodes_box_{stability_algo}.pdf", no_xticks=False)
         
         ax = (by_edge.cumsum() / len(by_edge)).plot(title=f"Cumulative Edge-Bag Similarity [0.0-1.0] Scores\n({stability_algo}[{T1:.2f}]: {edge_ratio:.2%})", ylim=YRANGE)
         save_ax_pdf(ax, f"{csv_stem}_edges_sum_{stability_algo}.pdf")
-        ax = by_edge.plot.box(title=f"Edge-Bag Similarity [0.0-1.0] Score Distributions\n({stability_algo}[{T1:.2f}]: {edge_ratio:.2%})", ylim=YRANGE)
-        save_ax_pdf(ax, f"{csv_stem}_edges_box_{stability_algo}.pdf", no_xticks=False)
+        #ax = by_edge.plot.box(title=f"Edge-Bag Similarity [0.0-1.0] Score Distributions\n({stability_algo}[{T1:.2f}]: {edge_ratio:.2%})", ylim=YRANGE)
+        #save_ax_pdf(ax, f"{csv_stem}_edges_box_{stability_algo}.pdf", no_xticks=False)
 
 
 if __name__ == "__main__":
