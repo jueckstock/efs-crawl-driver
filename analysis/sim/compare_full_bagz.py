566ad2d62820c8396814148c851ddf3427284022
Jordan Jueckstock <jjuecks@ncsu.edu>
2020-10-05 21:47:30 -0400

    hack it up for edge-type filtering

--- analysis/sim/compare_full_bagz.py
+++ analysis/sim/compare_full_bagz.py
@@ -11,13 +11,50 @@ from collections import defaultdict
 import numpy as np
 from multiset import Multiset
 
-FIXED_NODE_SET = os.environ.get("FIXED_NODE_SET", None)
+FIXED_EDGE_SET = os.environ.get("FIXED_EDGE_SET", None)
 SET_CLASS = Multiset if os.environ.get("MULTISET", False) else set
 
 RE_NODE_PATTERN = re.compile("^(\w+)(?:\[|$)")
-RE_EDGE_PATTERN = re.compile("^(\w+)\[?.*?->(\w+)(?:\[|$)")
+RE_EDGE_PATTERN = re.compile("^(\w+):")
 
-ALL_NODE_TYPES = ['DomRoot', 'LocalStorage', 'SessionStorage', 'Resource', 'WebAPI', 'Script', 'JsBuiltin', 'CookieJar', 'Html', 'FrameOwner', 'Text']
+ALL_NODE_TYPES = [
+    "RemoteFrame",
+    "Resource",
+    "WebAPI",
+    "JsBuiltin",
+    "Html",
+    "Text",
+    "DomRoot",
+    "FrameOwner",
+    "LocalStorage",
+    "SessionStorage",
+    "CookieJar",
+    "Script",
+    "Parser",
+]
+
+ALL_EDGE_TYPES = [
+    "TextChange",
+    "CreateNode",
+    "InsertNode",
+    "RemoveNode",
+    "DeleteNode",
+    "JsCall",
+    "Execute",
+    "RequestStart",
+    "RequestError",
+    "RequestComplete",
+    "AddEventListener",
+    "RemoveEventListener",
+    "EventListener",
+    "StorageSet",
+    "ReadStorageCall",
+    "DeleteStorage",
+    "ClearStorage",
+    "ExecuteFromAttribute",
+    "SetAttribute",
+    "DeleteAttribute",
+]
 
 
 def jaccard_index(a: SET_CLASS, b: SET_CLASS) -> float:
@@ -26,36 +63,32 @@ def jaccard_index(a: SET_CLASS, b: SET_CLASS) -> float:
     return num / den if den > 0 else np.nan
 
 
-def ok_node(match, allowed_nodes: list = ALL_NODE_TYPES) -> bool:
-    kind = match.group(1)
-    return kind in allowed_nodes
+def ok_match(match, allowed_things: list) -> bool:
+    return match and (match.group(1) in allowed_things)
 
 
-def ok_edge(match, allowed_nodes: list = ALL_NODE_TYPES) -> bool:
-    kind1 = match.group(1)
-    kind2 = match.group(2)
-    return (kind1 in allowed_nodes) and (kind2 in allowed_nodes)
-
-
-def load_set(file_name: str, allowed_nodes: list = ALL_NODE_TYPES) -> SET_CLASS:
+def load_set(
+    file_name: str,
+    allowed_nodes: list = ALL_NODE_TYPES,
+    allowed_edges: list = ALL_EDGE_TYPES,
+) -> SET_CLASS:
     s = SET_CLASS()
     regex = None
-    ok_func = None
     with open(file_name, "rt", encoding="utf8") as fd:
         line = next(fd).strip()
         m = RE_EDGE_PATTERN.match(line)
         if m:
             regex = RE_EDGE_PATTERN
-            ok_func = ok_edge
+            allowed_things = allowed_edges
         else:
             m = RE_NODE_PATTERN.match(line)
             if m:
                 regex = RE_NODE_PATTERN
-                ok_func = ok_node
+                allowed_things = allowed_nodes
             else:
                 raise ValueError(f"unknown set member type ({line})")
-        
-        if ok_func(m, allowed_nodes):
+
+        if ok_match(m, allowed_things):
             s.add(line)
 
         for line in fd:
@@ -63,56 +96,63 @@ def load_set(file_name: str, allowed_nodes: list = ALL_NODE_TYPES) -> SET_CLASS:
             m = regex.match(line)
             if not m:
                 raise ValueError(f"unexpected set member type ({line})")
-            #print(m.groups(), file=sys.stderr)
-            if ok_func(m, allowed_nodes):
+            # print(m.groups(), file=sys.stderr)
+            if ok_match(m, allowed_things):
                 s.add(line)
-    
+
     return s
 
 
-def process_directories(url_files: list, allowed_node_types: list):
+def process_directories(url_files: list, allowed_node_types: list = ALL_NODE_TYPES, allowed_edge_types: list = ALL_EDGE_TYPES):
     for url_file in url_files:
-        #print(url_file, file=sys.stderr)
+        # print(url_file, file=sys.stderr)
         *_, hostname, crawl_url_tag, _, _ = url_file.split(os.sep)
         site_tag = os.path.join(hostname, crawl_url_tag)
         with open(url_file, "rt", encoding="utf8") as fd:
             frame_url = fd.read().strip()
-        
+
         work_dir = os.path.dirname(url_file)
-        nbags = {}
-        for bf in glob.glob(os.path.join(work_dir, "*.nbag")):
-            p = os.path.splitext(os.path.basename(bf))[0]
-            nbags[p] = load_set(bf, allowed_node_types)
         
+        #nbags = {}
+        #for bf in glob.glob(os.path.join(work_dir, "*.nbag")):
+        #    p = os.path.splitext(os.path.basename(bf))[0]
+        #    nbags[p] = load_set(bf, allowed_node_types)
+
         ebags = {}
         for bf in glob.glob(os.path.join(work_dir, "*.ebag")):
             p = os.path.splitext(os.path.basename(bf))[0]
-            ebags[p] = load_set(bf, allowed_node_types)
+            ebags[p] = load_set(bf, allowed_edge_types)
 
-        profiles = list(sorted(nbags.keys()))
-        for p1, p2 in itertools.combinations(profiles, 2):
-            nji = jaccard_index(nbags[p1], nbags[p2])
+        profiles = list(sorted(ebags.keys()))
+        #for p1, p2 in itertools.combinations(profiles, 2):
+        p2 = profiles[-1]
+        for p1 in profiles[:-1]:
+            #nji = jaccard_index(nbags[p1], nbags[p2])
             eji = jaccard_index(ebags[p1], ebags[p2])
-            yield (site_tag, frame_url, p1, p2, nji, eji)
+            #yield (site_tag, frame_url, p1, p2, nji, eji)
+            yield (site_tag, frame_url, p1, p2, eji)
 
 
 def main(argv):
     url_files = argv[1:]
     wtr = csv.writer(sys.stdout, lineterminator="\n")
-    if FIXED_NODE_SET is not None:
-        requested_node_types = set(FIXED_NODE_SET.split('/'))
-        ordered_node_types = [t for t in ALL_NODE_TYPES if t in requested_node_types]
-        
-        wtr.writerow(['site_tag', 'frame_url', 'p1', 'p2', 'node_jaccard', 'edge_jaccard'])
-        for stats_row in process_directories(url_files, ordered_node_types):
-            wtr.writerow(stats_row)
-    else:
-        wtr.writerow(['node_mask', 'site_tag', 'frame_url', 'p1', 'p2', 'node_jaccard', 'edge_jaccard'])
-        for allowed_bits in itertools.product(range(2), repeat=len(ALL_NODE_TYPES)):
-            allowed_node_mask = ''.join(map(str, allowed_bits))
-            allowed_node_types = [ALL_NODE_TYPES[i] for i, b in enumerate(allowed_bits) if b]
-            for stats_row in process_directories(url_files, allowed_node_types):
-                wtr.writerow((allowed_node_mask,) + stats_row)
+    
+    wtr.writerow([
+            "edge_mask",
+            "site_tag",
+            "frame_url",
+            "p1",
+            "p2",
+            #"node_jaccard",
+            "edge_jaccard",
+        ])
+    for allowed_bits in itertools.product(range(2), repeat=len(ALL_EDGE_TYPES)):
+        allowed_edge_mask = "".join(map(str, allowed_bits))
+        allowed_edge_types = [
+            ALL_EDGE_TYPES[i] for i, b in enumerate(allowed_bits) if b
+        ]
+        for stats_row in process_directories(url_files, ALL_NODE_TYPES, allowed_edge_types):
+            wtr.writerow((allowed_edge_mask,) + stats_row)
 
 
 if __name__ == "__main__":
